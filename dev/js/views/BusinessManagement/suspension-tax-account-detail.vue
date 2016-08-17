71a92480cec8ba9db03c3fcb214626d51dc6f25e
ZhaoJunHan <zhaojunhan@kashuo.com>
2016-08-17 17:16:57 +0800

    CWXT-915页面：暂扣税金账户 CWXT-917页面：合计

---
+++ dev/js/views/BusinessManagement/suspension-tax-account-detail.vue
@@ -0,0 +1,302 @@
+<template>
+    <index :title="'暂扣税金账户明细'"
+           :ptitle="'商户管理'"
+           :p2title="'商户列表'"
+           :hname="'business-lists'"
+           :h2name="'business-lists'"
+           :isshow="'isshow'">
+        <div class="content" slot="content">
+            <div class="panel panel-flat">
+                <div class="panel-heading">
+                    <form class="form-inline manage-form">
+                        <div class="form-group">
+                            <a @click="recharge()" data-toggle="modal" data-target="#modal_submit" class="btn btn-info">充值</a>
+                        </div>
+                        <div class="form-group">
+                            <select class="form-control" v-model="dateS">
+                                <option value="0">昨天</option>
+                                <option value="1">最近一周</option>
+                                <option value="2">最近一个月</option>
+                                <option value="3">最近三个月</option>
+                                <option value="4">自定义时间</option>
+                            </select>
+                        </div>
+                        <div class="form-group" v-show="dateS==4">
+                            <datepicker :readonly="true" :value.sync="defaultData.startDate"
+                                        format="YYYY-MM-DD"></datepicker>
+                            至
+                            <datepicker :readonly="true" :value.sync="defaultData.endDate"
+                                        format="YYYY-MM-DD"></datepicker>
+                        </div>
+                        <div class="form-group">
+                            <input type="number" class="form-control" v-model="defaultData.orderID" placeholder="订单号"  onKeyUp="this.value=this.value.replace(/\D/g,'')" onafterpaste="this.value=this.value.replace(/\D/g,'')" >
+                        </div>
+                        <div class="form-group">
+                            <select class="form-control" v-model="defaultData.payType">
+                                <option value="">请选择付款方式</option>
+                                <option value="2">备付金</option>
+                                <option value="1">预付款</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <select class="form-control" v-model="defaultData.status">
+                                <option value="">请选择状态</option>
+                                <option value="2">对账成功</option>
+                                <option value="1">等待对账</option>
+                                <option value="1">等待划付</option>
+                                <option value="1">划付失败</option>
+                                <option value="1">已关闭</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control" v-model="defaultData.remarks" placeholder="备注">
+                        </div>
+                        <div class="form-group">
+                            <a class="btn btn-info" @click="initList">查询</a>
+                        </div>
+                        <div class="form-group">
+                            <a class="btn btn-info" @click="excel">导出</a>
+                        </div>
+                    </form>
+                </div>
+                <div style="margin: 0 0 20px 20px;font-size: 18px;">
+                    <span>商户名：</span><span style="margin-right: 10px;">{{balance.merchantName}}</span>
+                    <span>账户名：</span><span style="margin-right: 10px;">{{balance.accountName}}</span>
+                    <span>账户余额：</span><span style="margin-right: 10px;">{{balance.balanceAmount }}</span>
+                </div>
+                <div v-if="zdlists.length>0" id="DataTables_Table_0_wrapper" class="dataTables_wrapper no-footer">
+                    <div class="datatable-scroll">
+                        <table id="table1" class="table datatable-selection-single dataTable no-footer">
+                            <thead>
+                            <tr role="row">
+                                <th>订单号</th>
+                                <th>收入</th>
+                                <th>支出</th>
+                                <th>付款方式 </th>
+                                <th>状态</th>
+                                <th>交易时间</th>
+                                <th>操作</th>
+                                <th>备注</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr role="row" v-for="(index,trlist) in zdlists">
+                                <td>{{trlist.orderID }}</td>
+                                <td>{{trlist.incomeAmount/100 | currency ''}}</td>
+                                <td>{{trlist.payoutAmount/100 | currency ''}}</td>
+                                <td>
+                                    <template v-if="trlist.payType==1">备付金</template>
+                                    <template v-if="trlist.payType==2">预付款</template>
+                                </td>
+                                <td>
+                                    <template v-if="trlist.status==1">备付金</template>
+                                    <template v-if="trlist.status==2">预付款</template>
+                                </td>
+                                <td>{{trlist.tradeTime  | datetimes}}</td>
+                                <td>
+                                    <a href="{{origin}}/file/download/{{n.certificateID}}">下载</a>
+                                </td>
+                                <td>{{trlist.remarks}}</td>
+                            </tr>
+                            <tr role="row">
+                                <td></td>
+                                <td>{{total.incomeAmount/100 | currency ''}}</td>
+                                <td>{{total.payoutAmount/100 | currency ''}}</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="datatable-footer">
+                        <page :all="defaultData.pageTotal"
+                              :cur.sync="defaultData.pageIndex"
+                              :page_size.sync="defaultData.pageSize">
+                        </page>
+                    </div>
+                </div>
+                <div style="padding: 30px;font-size: 16px;text-align: center" v-else>
+                    未找到数据
+                </div>
+
+                <div data-backdrop="static"  id="modal_applyPay" class="modal fade" style="display: none;">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal">×</button>
+                                <h5 class="modal-title">申请提现</h5>
+                            </div>
+                            <div class="modal-body">
+                                <div class="form-group">
+                                    <label class="control-label">税金池余额：{{redata.suspensionTaxAmount/100 | currency ''}} 元</label>
+                                    <label class="control-label">可提现金额：{{redata.withdrawCashAmount/100 | currency ''}} 元</label>
+                                </div>
+                                <div class="form-group">
+                                    <label class="control-label"><i>*</i>付款方式：</label>
+                                    <select class="form-control" v-model="applyData.payType">
+                                        <option value="1">备付金账户</option>
+                                        <option value="0">商户预付款账户</option>
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <label style="position: relative;top: -95px;" class="control-label">备注：</label>
+                                    <textarea rows="5" cols="5" class="form-control" v-model="applyData.remarks"></textarea>
+                                </div>
+                                <div class="form-group tc">
+                                    <button  type="button" @click="applyPayTrue" class="btn btn-primary">申请提现</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </index>
+</template>
+<style>
+
+</style>
+<script>
+    import model from '../../ajax/BusinessManagement/suspension-tax-account-detail'
+    import datepicker from '../components/datepicker.vue'
+    export default{
+        components:{
+            'datepicker': datepicker
+        },
+        data(){
+            this.model =model(this)
+            return{
+                origin:window.origin,
+                defaultData:{
+                    'subsidyAccountID': '',
+                    'orderID': '',
+                    'startDate': '',
+                    'endDate': '',
+                    'payType': '',
+                    'status': '',
+                    'remarks': '',
+                    'pageTotal': 1,
+                    'pageIndex': 1,
+                    'pageSize': 10,
+                    mid:JSON.parse(sessionStorage.getItem('userData')).authToken
+                },
+                zdlists:[],
+                total:{
+                    'incomeAmount': '',
+                    'payoutAmount': '',
+                },
+                balance:{
+                    accountName:'',
+                    merchantName:'',
+                    balanceAmount:''
+                },
+                redata:{
+                    suspensionTaxAmount:'',
+                    withdrawCashAmount:''
+                },
+                applyData:{
+                    remarks:'',
+                    payType:'1'
+                },
+            }
+        },
+        methods:{
+            // *** 请求账户数据
+            getZlists(){
+                let data=this.defaultData
+                if(sessionStorage.getItem('isHttpin')==1)return;
+                if(data.endValue<data.startValue){
+                    this.defaultData.endValue=b;
+                    data.startValue=a;
+                    data.endValue=b;
+                }
+                this.model.suspensionTaxAccountDetail_list(data)
+                        .then((response)=>{
+                            // *** 判断请求是否成功如若成功则填充数据到模型
+                            if(response.data.code==0){
+                                this.$set('zdlists', response.data.data)
+                                this.defaultData.pageTotal=response.data.total
+                            }
+                        });
+                this.model.suspensionTaxAccountDetail_total(data)
+                        .then((response)=>{
+                            // *** 判断请求是否成功如若成功则填充数据到模型
+                            if(response.data.code==0){
+                                this.$set('total', response.data.data)
+                            }
+                        });
+            },
+            initList(){
+                if(this.defaultData.pageIndex==1){
+                    this. getZlists();
+                    return;
+                }
+                this.defaultData.pageIndex=1;
+            },
+            excel(){
+                if(sessionStorage.getItem('isHttpin')==1)return;
+                //初始化
+                window.open(window.origin+this.$API.activityManage+ $.param(this.defaultData));
+            },
+            applyPay(){
+                this.model.subsidyAccount_info(this.defaultData)
+                        .then((response)=>{
+                            // *** 判断请求是否成功如若成功则填充数据到模型
+                            if(response.data.code==0){
+                                this.$set('redata', response.data.data);
+                                $('#modal_applyPay').modal('show');
+                            }
+                        });
+            },
+            applyPayTrue(){
+                if(sessionStorage.getItem('isHttpin')==1)return;
+                this.model.subsidyAccount_applyPay(this.applyPay)
+                        .then((response)=>{
+                            if(response.data.code == 0){
+                                dialogs('success','已充值！');
+                                this.initList();
+                            }
+                        });
+            },
+            getTime(){
+                this.checkForm.startDate=init_date(this.dateS)[0];
+                this.checkForm.endDate=init_date(this.dateS)[1];
+            },
+        },
+        watch:{
+            'defaultData.pageIndex':{
+                handler:(val,oldVal)=>{
+                    this.getZlists()
+                },
+                deep:true
+            },
+            'defaultData.pageSize':{
+                handler:(val,oldVal)=>{
+                    this.getZlists()
+                },
+                deep:true
+            },
+            dateS(){
+                this.getTime();
+            }
+        },
+        ready(){
+            let vm=this;
+            (vm.$route.params.suspensionZHname==':suspensionZHname')?vm.defaultData.merchantID= '' : vm.defaultData.merchantID=vm.$route.params.merchantID;
+            (vm.$route.params.suspensionSHname==':suspensionSHname')? vm.balance.merchantName='' : vm.balance.merchantName=vm.$route.params.merchantName;
+            (vm.$route.params.suspensionZHbalance==':suspensionZHbalance')? vm.balance.merchantOperationID='' : vm.balance.merchantOperationID=vm.$route.params.merchantOperationID;
+            (vm.$route.params.suspensionBTid==':suspensionBTid')? vm.balance.merchantOperationID='' : vm.balance.merchantOperationID=vm.$route.params.merchantOperationID;
+            vm.getTime();
+            vm.getZlists();
+            $('#modal_recharge').on('hidden.bs.modal', function () {
+                $('body').css('padding-right',0);
+                vm.uploadText='';
+                vm.recharge.certificatesID='';
+            })
+        }
+    }
+</script>
