f18b33238856a739cbd2c99c33400690e9048946
ZhaoJunHan <zhaojunhan@kashuo.com>
2016-09-13 11:25:51 +0800

    ~~~

---
+++ dev/js/views/components/BackgroundMask.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="k-mask" @click="$emit('kclick', $event)">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default{
+    data () {
+      return { }
+    }
+  }
+</script>
+
+<style lang="sass">
+  @import "../../../style/bootstrap";
+
+  // 遮罩样式
+  .k-mask {
+    position: fixed;top: -50px;left: 0;right: 0;bottom: 0;
+    z-index: 108;
+    background: rgba(#000, .3);
+  }
+</style>
