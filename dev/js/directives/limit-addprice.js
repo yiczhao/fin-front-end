0e1af10c929205d854353b85dd359ee1b3a064d6
ZhaoJunHan <zhaojunhan@kashuo.com>
2016-10-19 10:24:21 +0800

    CWXT-1392【其他信息】回款账期中文输入法输入字符保存报错

--- dev/js/directives/limit-addprice.js
+++ dev/js/directives/limit-addprice.js
@@ -3,19 +3,16 @@
  *
  */
 export default {
-
 	twoWay: true,
 	bind: function () {
-
 		this.handler = function () {
 			// 将数据写回 vm
 			// 如果指令这样绑定 v-example="a.b.c"
 			// 它将用给定值设置 `vm.a.b.c`
-			let num=this.el.value.replace(/[^\d+\.\?$]/g,'')
+			let num=this.el.value.replace(/[^\d+\.\?$]/g,'');
+			this.el.value = num;
 			this.set(num)
 		}.bind(this)
-
-		this.el.addEventListener('keyup', this.handler)
-
+		this.el.addEventListener('input', this.handler)
 	}
 }
