cda1e8040bf60b1eb28927cf1e94b300967d3692
洪钢 <honggang@kashuo.com>
2016-10-31 15:16:40 +0800

    移除多余代码，修改版本号

--- dev/js/views/uploadForm.vue
+++ dev/js/views/uploadForm.vue
@@ -20,15 +20,6 @@
 									</td>
 									<td><input type="button" class="btn btn-primary" value="提交" @click="submitTime($event)"/></td>
 								</tr>
-								<tr>
-									<td>
-										<input type="button" class="btn btn-primary" value="补贴账户数据" data-toggle="modal" data-target="#modal_subsidy_account_data"/>
-										<input type="button" class="btn btn-primary" value="江西建行数据修复" data-toggle="modal" data-target="#modal_waring"/>
-									</td>
-									<td>
-										<input type="button" class="btn btn-primary" value="测试" @click="testBank"/>
-									</td>
-								</tr>
                                 <tr>
                                     <td>
                                         开始日期：<datepicker  :readonly="true" :width="'150px'" :value.sync="startdateStr" format="YYYY-MM-DD"></datepicker>
@@ -41,27 +32,11 @@
                                 </tr>
 								<tr>
 									<td>
-										<input type="text" class="form-control" v-model="startID" placeholder="开始ID"/>
-										<input type="text" class="form-control" v-model="endID" placeholder="结束ID"/>
-									</td>
-									<td>
-										<input type="button" class="btn btn-primary" value="提交" @click="generateDataYesterday($event)"/>
-									</td>
-								</tr>
-								<tr>
-									<td>
 										<input type="text" class="form-control" v-model="flowID" placeholder="flowID"/>
 										<input type="text" class="form-control" v-model="serialNumber" placeholder="交易流水号"/>
 									</td>
 									<td>
 										<input type="button" class="btn btn-primary" value="提交" @click="revokeTradeDetail($event)"/>
-									</td>
-								</tr>
-								<tr>
-									<td>
-										<input type="button" class="btn btn-primary" value="生成预付款账户明细" data-toggle="modal" data-target="#modal_advance_account_waring"/>
-									</td>
-									<td>
 									</td>
 								</tr>
 								<tr>
@@ -73,54 +48,6 @@
 									</td>
 								</tr>
                             </table>
-							<div data-backdrop="static"  id="modal_waring" class="modal fade" style="display: none;">
-								<div class="modal-dialog">
-									<div class="modal-content">
-										<div class="modal-header">
-											<button type="button" class="close" data-dismiss="modal">×</button>
-											<h5 class="modal-title">你确认修复数据？</h5>
-										</div>
-										<div class="modal-body">
-											<div class="form-group tc">
-												<button type="button" @click="submitSave" class="btn btn-primary">确认</button>
-												<button type="button" class="btn btn-gray" data-dismiss="modal">取消</button>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div data-backdrop="static"  id="modal_subsidy_account_data" class="modal fade" style="display: none;">
-								<div class="modal-dialog">
-									<div class="modal-content">
-										<div class="modal-header">
-											<button type="button" class="close" data-dismiss="modal">×</button>
-											<h5 class="modal-title">你确定生成数据么？</h5>
-										</div>
-										<div class="modal-body">
-											<div class="form-group tc">
-												<button type="button" @click="generateSubsidyAccountData" class="btn btn-primary">确认</button>
-												<button type="button" class="btn btn-gray" data-dismiss="modal">取消</button>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div data-backdrop="static"  id="modal_advance_account_waring" class="modal fade" style="display: none;">
-								<div class="modal-dialog">
-									<div class="modal-content">
-										<div class="modal-header">
-											<button type="button" class="close" data-dismiss="modal">×</button>
-											<h5 class="modal-title">你确认生成预付款账户明细吗？</h5>
-										</div>
-										<div class="modal-body">
-											<div class="form-group tc">
-												<button type="button" @click="createAdvancePaymentAccountDetail" class="btn btn-primary">确认</button>
-												<button type="button" class="btn btn-gray" data-dismiss="modal">取消</button>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
 						</form>
 					</div>
 				 </div>
@@ -190,34 +117,15 @@
 					}
 					this.$http.post('./dev/tool/upload',datas)
 							.then((response)=>{
-						dialogs('success','上传成功！');
+						dialogs('success',response.data.message);
 				})
 			},
 			submitTime(e){
 				if(sessionStorage.getItem('isHttpin')==1)return;
 					this.$http.post('./dev/tool/tradeDetail/rollback',this.dateStr)
 							.then((response)=>{
-									dialogs('success','回滚成功！');
+									dialogs('success',response.data.message);
 							})
-			},
-			submitSave(){
-				this.$http.post('./dev/tool/tradeDetail/JXCCB/rollback')
-						.then((response)=>{
-								dialogs('success','回滚成功！');
-						})
-			},
-			generateSubsidyAccountData(){
-				this.$http.post('./dev/tool/subsidy_account/generateData')
-						.then((response)=> {
-							dialogs('success', '生成成功！');
-						})
-			},
-			testBank(){
-				this.$http.post('./dev/tool/testBank')
-						.then(
-								(response)=>{
-									dialogs('success','请求成功');
-				})
 			},
             reserveCashDetail(e){
                 if(sessionStorage.getItem('isHttpin')==1)return;
@@ -228,20 +136,9 @@
                 }
                 this.$http.post('./dev/tool/reserveCashDetail',data)
                         .then((response)=>{
-                            dialogs('success','处理成功！');
+                            dialogs('success',response.data.message);
                         })
             },
-			generateDataYesterday(e){
-				if(sessionStorage.getItem('isHttpin')==1)return;
-				let data={
-					startID :this.startID,
-					endID :this.endID
-				}
-				this.$http.post('./dev/tool/subsidy_account/generateDataYesterday',data)
-						.then((response)=>{
-								dialogs('success','处理成功！');
-						})
-			},
 			revokeTradeDetail(e){
 				if(this.flowID!=''&&this.serialNumber!=''){
 					dialogs('error','只能传入一个参数！');
@@ -253,13 +150,7 @@
 				}
 				this.$http.post('./dev/tool/tradeDetail/revoke',data)
 						.then((response)=>{
-							dialogs('success','撤销成功！');
-						})
-			},
-			createAdvancePaymentAccountDetail(e){
-				this.$http.post('./dev/tool/advancePayment/accountDetail/create')
-						.then((response)=>{
-					dialogs('success','生成成功！');
+							dialogs('success',response.data.message);
 						})
 			},
 			excuteJob(){
