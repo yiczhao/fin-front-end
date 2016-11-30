8cf8882b719a951ecf1adefa320f9314c5473bcd
ZhaoJunHan <zhaojunhan@kashuo.com>
2016-11-30 10:29:34 +0800

    CWXT-1631活动其它信息优化

--- dev/js/views/Activity/activity-lists.vue
+++ dev/js/views/Activity/activity-lists.vue
@@ -92,7 +92,7 @@
                                 <td>{{trlist.commissionAmount/100 | currency ''}}</td>
                                 <td><a data-ksa="trade_detail_manage.search" v-link="{'name':'trade-info','params':{'activityOperationID':trlist.operationID,'tradeCompanyId':trlist.subCompanyID}}">交易明细</a></td>
                                 <td>
-                                    <a data-ksa="activity_manage.config" class="mr20" @link="{'name':'activity-formulae','params':{'activityID':trlist.id, 'subCompanyID':trlist.subCompanyID, 'formulaeID':trlist.operationID, 'formulaeName':trlist.name}}">计算公式</a>
+                                    <a data-ksa="activity_manage.config" class="mr20" v-link="{'name':'activity-formulae','params':{'activityID':trlist.id, 'subCompanyID':trlist.subCompanyID, 'formulaeID':trlist.operationID, 'formulaeName':trlist.name}}">计算公式</a>
                                     <a data-ksa="activity_manage.config" @click="otherInfo(trlist.subCompanyID,trlist.id)">其他信息</a>
                                 </td>
                             </tr>
