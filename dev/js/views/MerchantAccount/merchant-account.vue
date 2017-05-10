5d94286ed8df112c20fa584d87d987143d6912de
ZhaoJunHan <zhaojunhan@kashuo.com>
2017-05-10 13:26:31 +0800

    CWXT-2165 【商户账户】【报表管理】【三方管理】模块分公司下拉框显示重复信息

--- dev/js/views/MerchantAccount/merchant-account.vue
+++ dev/js/views/MerchantAccount/merchant-account.vue
@@ -313,7 +313,7 @@
 					});
 			},
 			getCompanyList(){
-				this.$common_model.getcompany(data)
+				this.$common_model.getcompany()
 					.then((response) => {
 						// *** 判断请求是否成功如若成功则填充数据到模型
 						(response.data.code == 0) ? this.$set('companyList', response.data.data) : null;
--- dev/js/views/ReportForm/allopatry-distribution-total.vue
+++ dev/js/views/ReportForm/allopatry-distribution-total.vue
@@ -201,14 +201,14 @@
                             this.$set('companylists', response.data.data)
                         }
                     });
-                this.model.getSearchBusiness(data)
+                this.model.getSearchBusiness()
                     .then((response)=>{
                         // *** 判断请求是否成功如若成功则填充数据到模型
                         if(response.data.code==0){
                             this.$set('commonbusinessList', response.data.data)
                         }
                     });
-                this.model.snPartnerList(data)
+                this.model.snPartnerList()
                     .then((response)=>{
                         // *** 判断请求是否成功如若成功则填充数据到模型
                         if(response.data.code==0){
