4b8d1ffbe5e23bc24a71ae58c75d8ad37b7da22a
ZhaoJunHan <zhaojunhan@kashuo.com>
2017-05-12 17:17:13 +0800

    CWXT-2197 报表、活动收入成本明细模块添加权限

--- dev/js/views/ReportForm/expense-management.vue
+++ dev/js/views/ReportForm/expense-management.vue
@@ -6,13 +6,13 @@
         <div class="content expense-management" slot="content">
            	<div class="panel panel-flat">
            	 	<ul class="tab-bor">
-                    <li data-ksa="target_total.search"     ><a v-link="{name:'financial-index-total'}">财务指标分析表（总）</a></li>
-                    <li data-ksa="target_branch.search"    ><a v-link="{name:'financial-index-branch'}">财务指标分析表（分）</a></li>
-                    <li data-ksa="ranking.search"          ><a v-link="{name:'branch-financial-indicators'}">财务指标排行</a></li>
-                    <li data-ksa="cost"                    class="active"><a>费用管理</a></li>
-                    <li data-ksa="target_manage.search"    ><a v-link="{name:'index-management'}">指标管理</a></li>
-                    <li data-ksa="cost.search"             ><a v-link="{name:'departmental-expense-detail'}">部门费用明细</a></li>
-                    <li data-ksa="activation_detail.search"><a v-link="{name:'allopatry-distribution-total'}">异地激活收入成本分配总表</a></li>
+                    <li data-ksa="target_total"><a v-link="{name:'financial-index-total'}">财务指标分析表（总）</a></li>
+                    <li data-ksa="target_branch"><a v-link="{name:'financial-index-branch'}">财务指标分析表（分）</a></li>
+                    <li data-ksa="ranking"><a v-link="{name:'branch-financial-indicators'}">财务指标排行</a></li>
+                    <li data-ksa="cost" class="active"><a>费用管理</a></li>
+                    <li data-ksa="target_manage"><a v-link="{name:'index-management'}">指标管理</a></li>
+                    <li data-ksa="cost"><a v-link="{name:'departmental-expense-detail'}">部门费用明细</a></li>
+                    <li data-ksa="activation_detail"><a v-link="{name:'allopatry-distribution-total'}">异地激活收入成本分配总表</a></li>
                 </ul>
                 <div class="heading">
                     <div class="heading-left">
